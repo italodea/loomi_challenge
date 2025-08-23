cded2b02ef2fc409eda629238124050eed403c11
Ítalo de Azevedo <italdoeazevedo99@gmail.com>
2025-08-22 23:55:39 -0300

    Improve screen responsiveness

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -31,98 +31,112 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(toolbarHeight: 0),
       body: SingleChildScrollView(
         padding: const EdgeInsets.only(right: 12, left: 12, top: 40),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Center(
-                child: Image.asset(
-                  'assets/img/logo_minimal.png',
-                  width: 40,
-                  height: 40,
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                "Welcome Back",
-                style: Theme.of(context).textTheme.titleLarge,
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 10),
-              Text(
-                "Look who is here!",
-                style: Theme.of(context).textTheme.bodyMedium,
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 30),
-              CustomTextField(
-                title: "Email",
-                hintText: "Enter your email",
-                inputFormatters: FieldMasks.formatEmail(),
-                validator: FieldValidations.validateEmail,
-                controller: _emailController,
-              ),
-              CustomTextField(
-                title: "Password",
-                hintText: "Enter your password",
-                obscureText: true,
-                controller: _passwordController,
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {
-                    Get.toNamed('/forgot-password');
-                  },
-                  child: Text(
-                    "Forgot Password?",
-                    style: TextStyle(color: Theme.of(context).primaryColor),
+        child: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 500),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Center(
+                    child: Image.asset(
+                      'assets/img/logo_minimal.png',
+                      width: 40,
+                      height: 40,
+                    ),
                   ),
-                ),
-              ),
-              SizedBox(height: 30),
-              CustomButton(
-                label: "Login",
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        title: Text('Login Successful'),
-                        content: Text('You have logged in successfully!'),
-                        actions: [
-                          TextButton(
-                            onPressed: () => Navigator.of(context).pop(),
-                            child: Text('OK'),
-                          ),
-                        ],
+                  SizedBox(height: 20),
+                  Text(
+                    "Welcome Back",
+                    style: Theme.of(context).textTheme.titleLarge,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Look who is here!",
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 30),
+                  CustomTextField(
+                    title: "Email",
+                    hintText: "Enter your email",
+                    inputFormatters: FieldMasks.formatEmail(),
+                    validator: FieldValidations.validateEmail,
+                    controller: _emailController,
+                  ),
+                  CustomTextField(
+                    title: "Password",
+                    hintText: "Enter your password",
+                    obscureText: true,
+                    controller: _passwordController,
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {
+                        Get.toNamed('/forgot-password');
+                      },
+                      child: Text(
+                        "Forgot Password?",
+                        style: TextStyle(color: Theme.of(context).primaryColor),
                       ),
-                    );
-                  }
-                },
-              ),
-              SizedBox(height: 20),
-              CustomDivider(text: "Or Sign in With"),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CustomSocialIconGoogle(),
-                  SizedBox(width: 16),
-                  CustomSocialIconApple()
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  CustomButton(
+                    label: "Login",
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: Text('Login Successful'),
+                            content: Text('You have logged in successfully!'),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: Text('OK'),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                  SizedBox(height: 20),
+                  CustomDivider(text: "Or Sign in With"),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomSocialIconGoogle(),
+                      SizedBox(width: 16),
+                      CustomSocialIconApple(),
+                    ],
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                        style: Theme.of(context).textTheme.labelMedium!.merge(
+                          TextStyle(color: AppTheme.secondaryDarkColor),
+                        ),
+                      ),
+                      CustomTextButton(
+                        label: "Sign Up",
+                        onPressed: () {
+                          Get.toNamed('/signup');
+                        },
+                      ),
+                    ],
+                  ),
                 ],
               ),
-              SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Don't have an account?", style: Theme.of(context).textTheme.labelMedium!.merge(TextStyle(color: AppTheme.secondaryDarkColor))),
-                  CustomTextButton(label: "Sign Up", onPressed: () {
-                    Get.toNamed('/signup');
-                  }),
-                ],
-              )
-            ],
+            ),
           ),
         ),
       ),
